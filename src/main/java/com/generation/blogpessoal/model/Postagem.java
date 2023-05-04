dc2064eb62811c6e4471ffeee53f13814492694c
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-05-04 17:13:31 -0300

    Implementação da Security no Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -18,19 +18,18 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-	
-//atributos da classe devem ser private, long - n postagens, nao se sabe quantas postagens o blog tera	
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+	//atributos da classe devem ser private, long - n postagens, nao se sabe quantas postagens o blog tera	
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 	
-	@NotBlank(message = "O atributo 'título' é obrigatório.")
-	@Size(min = 5, max = 100, message = "O atributo 'título' deve ter entre 5 e 100 caracteres.")
+	@NotBlank(message = "O atributo 'título' é obrigatório.") 
+	@Size(min = 5, max = 100, message = "O atributo 'título' deve conter no mínimo 05 e no máximo 100 caracteres.")
 	private String titulo;
 	
 	@NotBlank(message = "O atributo 'texto' é obrigatório.")
-	@Size(min = 10, max = 1000, message = "O atributo 'texto' deve ter entre 5 e 100 caracteres.")
+	@Size(min = 10, max = 1000, message = "O atributo 'texto' deve conter no mínimo 10 e no máximo 1000 caracteres.")
 	private String texto;
 	
 	@UpdateTimestamp
@@ -39,6 +38,42 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 
 	public Tema getTema() {
 		return tema;
@@ -48,38 +83,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 
-	public Long getId() {
-		return id;
+	public Usuario getUsuario() {
+		return usuario;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getTexto() {
-		return texto;
-	}
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-
-	public LocalDateTime getData() {
-		return data;
-	}
-
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-	
-	
-
+    
 }
